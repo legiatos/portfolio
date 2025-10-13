1471307a4b8a18c66001fe5b0030c087b7cee4d2
Simon Krumb <krumbs@hostname.iut2.upmf-grenoble.fr>
2025-10-13 10:39:12 +0200

    accueil: creation dynamique des projets, header sticky

--- index.php
+++ index.php
@@ -1,62 +1,59 @@
-<?php
-// Charger le fichier JSON
-$json = file_get_contents("./data/projets.json");
-$projects = json_decode($json, true);
-?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mes Projets</title>
-    <link rel="stylesheet" href="../css/global.css">
-    <link rel="stylesheet" href="projets.css">
+    <title>Simon Krumb</title>
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/global.css">
 </head>
 <body>
     <header>
         <h1>Krumb Simon</h1>
-        <h2>Mes Projets</h2>
+        <h2>Étudiant BUT Informatique</h2>
     </header>
 
     <nav>
         <ul>
-            <li><a href="../">Accueil</a></li>
-            <li class="current"><a href="../projets/">Mon travail</a></li>
-            <li><a href="../qui-suis-je/">Qui suis-je?</a></li>
-            <li><a href="../contact/">Contact</a></li>
+            <li class="current"><a href="./">Accueil</a></li>
+            <li><a href="./projets/">Mon travail</a></li>
+            <li><a href="./qui-suis-je/">Qui suis-je?</a></li>
+            <li><a href="./contact/">Contact</a></li>
         </ul>
     </nav>
 
     <main>
         <div>
-            <?php foreach ($projects as $key => $proj): ?>
-                <article id="<?= htmlspecialchars($key) ?>">
-                    <a href="#<?= htmlspecialchars($key) ?>">
-                        <div>
-                            <h3>krumb@simon:~/ls "<?= htmlspecialchars($proj["nom"]) ?>_"</h3>
-                            <p>total <?= count($proj["resume"]) ?></p>
-                            <br>
-                            <p>Travail en groupe de <?= htmlspecialchars($proj["tailleGroupe"]) ?></p>
-                            <br>
-                            <ol>
-                                <?php foreach ($proj["resume"] as $r): ?>
-                                    <li><?= htmlspecialchars($r) ?></li>
-                                <?php endforeach; ?>
-                            </ol>
-                            <br>
-                            <ul>
-                                <?php foreach ($proj["competences"] as $c): ?>
-                                    <?php if ($c === ""): ?>
-                                        <br>
-                                    <?php else: ?>
-                                        <li><?= htmlspecialchars($c) ?></li>
-                                    <?php endif; ?>
-                                <?php endforeach; ?>
-                            </ul>
-                        </div>
-                    </a>
-                </article>
-            <?php endforeach; ?>
+            <?php
+            // Load JSON file
+            $jsonPath = './data/projets.json';
+            if (file_exists($jsonPath)) {
+                $jsonData = file_get_contents($jsonPath);
+                $projects = json_decode($jsonData, true);
+
+                $counter = 1;
+                foreach ($projects as $key => $project) {
+                    // Choose a fake terminal command
+                    $command = ($counter <= 2) ? 'cd' : 'ls';
+
+                    // Sanitize and output article
+                    $nom = htmlspecialchars($project['nom']);
+
+                    echo <<<HTML
+<article>
+    <a href="./projets/index.php#{$counter}">
+        <div>
+            <h3>krumb@simon:~/$command "$nom"_</h3>
+        </div>
+    </a>
+</article>
+HTML;
+                    $counter++;
+                }
+            } else {
+                echo "<p>Erreur : le fichier projets.json est introuvable.</p>";
+            }
+            ?>
         </div>
     </main>
 
